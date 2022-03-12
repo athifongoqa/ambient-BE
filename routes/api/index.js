988c5e5981bc4e45abb313f289986728ab31ec0a
Alexandre Costa <alexandre.costa@code.berlin>
2022-03-12 21:27:47 +0100

    fix: standardize users endpoints path to /users

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,49 +1,47 @@
-'use strict'
+"use strict";
 
-const User = require('../../models/user')
-const validator = require('validator')
+const User = require("../../models/user");
+const validator = require("validator");
 
 module.exports = async function (fastify, opts) {
-    fastify.register(require('fastify-prettier'),
-    {
-      alwaysOn: true
-    });
+  fastify.register(require("fastify-prettier"), {
+    alwaysOn: true,
+  });
 
-    fastify.get('/users', async function (req, reply) {
-      const users = await User.find()
-      reply.send({allUsers: users})
-    });
+  fastify.get("/users", async function (req, reply) {
+    const users = await User.find();
+    reply.send({ allUsers: users });
+  });
 
-    fastify.get('/users/:username', async function (req, reply) {
-      const username = req.params.username
-      let user = await User.findOne({username: username})
-      reply.send({requestedUser: user})
-    });
+  fastify.get("/users/:username", async function (req, reply) {
+    const username = req.params.username;
+    let user = await User.findOne({ username: username });
+    reply.send({ requestedUser: user });
+  });
 
-    fastify.post('/user', async (req, reply) => {
-      
-      // Validate Email
-      if (!validator.isEmail(req.body.email)) {
-        reply.send({message: 'Please use a valid email address.'})
-        return
-      }
-      
-      let user = new User(req.body)
-      let returnedUser = await user.save()
-      reply.send({addedUser: returnedUser}) 
-    });
+  fastify.post("/users", async (req, reply) => {
+    // Validate Email
+    if (!validator.isEmail(req.body.email)) {
+      reply.send({ message: "Please use a valid email address." });
+      return;
+    }
 
-    fastify.patch('/user/:id', async function (req, reply) {
-      const id = req.params.id
-      let updatedUser = await User.findByIdAndUpdate(id, req.body, {
-         new: true
-      })
-      reply.send({updatedUser: updatedUser})
-    });
+    let user = new User(req.body);
+    let returnedUser = await user.save();
+    reply.send({ addedUser: returnedUser });
+  });
 
-    fastify.delete('/user/:id', async function (req, reply) {
-      const id = req.params.id
-      let deletedUser = await User.findByIdAndDelete(id)
-      reply.send({message: `${deletedUser.id} has been deleted`})
+  fastify.patch("/users/:id", async function (req, reply) {
+    const id = req.params.id;
+    let updatedUser = await User.findByIdAndUpdate(id, req.body, {
+      new: true,
     });
-}
+    reply.send({ updatedUser: updatedUser });
+  });
+
+  fastify.delete("/users/:id", async function (req, reply) {
+    const id = req.params.id;
+    let deletedUser = await User.findByIdAndDelete(id);
+    reply.send({ message: `${deletedUser.id} has been deleted` });
+  });
+};
