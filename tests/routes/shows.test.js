76712c09672692e03b95f5739f0efe3f7a2509d0
Alexandre Costa <alexandre.costa@code.berlin>
2022-03-30 00:51:55 +0200

    test: get single show and get all shows

--- tests/routes/shows.test.js
+++ tests/routes/shows.test.js
@@ -1,29 +1,73 @@
 const mockingoose = require("mockingoose");
 const ShowModel = require("../../models/show.model.js");
-const { getShow } = require("../../controllers/shows.js");
+const { getShow, getShows } = require("../../controllers/shows.js");
 
 describe("Shows routes", () => {
-  describe("Shows routes", () => {
-    it("get single show return 1 show", async () => {
-      let value = null;
-      const show = {
+  it("Get single show", async () => {
+    // Arrange
+    const showInput = {
+      _id: "1234",
+      name: "Test Show Name",
+    };
+
+    const req = {
+      params: {
+        id: showInput._id,
+      },
+    };
+
+    const reply = {
+      send: jest.fn().mockImplementation((res) => {
+        response = res;
+      }),
+    };
+
+    mockingoose(ShowModel).toReturn(showInput, "findOne");
+
+    let response;
+
+    // Act
+    await getShow(req, reply);
+
+    // Assert
+    expect(response).toHaveProperty("name", "Test Show Name");
+    expect(response).toBeInstanceOf(Object);
+    expect(reply.send).toBeCalledTimes(1);
+  });
+
+  it("Get all shows", async () => {
+    // Arrange
+    const showsInput = [
+      {
         _id: "1234",
         name: "Test Show Name",
-        description: "Test Show Description",
-        type: "Test Show Type",
-      };
+      },
+      {
+        _id: "5678",
+        name: "Test Show Name 2",
+      },
+    ];
 
-      mockingoose(ShowModel).toReturn(show, "findOne");
+    const req = {};
 
-      const results = await getShow(
-        { params: { _id: "1234" } },
-        {
-          send: (show) => {
-            value = show;
-          },
-        }
-      );
-      expect(value).toHaveProperty("name", "Test Show Name");
-    });
+    const reply = {
+      send: jest.fn().mockImplementation((res) => {
+        response = res;
+      }),
+    };
+
+    mockingoose(ShowModel).toReturn(showsInput, "find");
+
+    let response;
+
+    // Act
+    await getShows(req, reply);
+
+    // Assert
+    expect(response).toHaveLength(2);
+    expect(response[0]).toHaveProperty("name", "Test Show Name");
+    expect(response[1]).toHaveProperty("name", "Test Show Name 2");
+    expect(response).toBeInstanceOf(Array);
+    expect(reply.send).toBeCalledTimes(1);
   });
 });
