a7c79db7656659518eac02791cfc46b99fd9874d
Alexandre Costa <alexandre.costa@code.berlin>
2022-03-29 18:48:26 +0200

    test: unit test working with mockingoose

--- tests/routes/shows.test.js
+++ tests/routes/shows.test.js
@@ -1,26 +1,29 @@
 const mockingoose = require("mockingoose");
 const ShowModel = require("../../models/show.model.js");
+const { getShow } = require("../../controllers/shows.js");
 
 describe("Shows routes", () => {
-  describe("Get single Show", () => {
-    it("should return the one show", async () => {
-      const _doc = {
-        creator_id: "987654321098765432109876",
-        name: "Mock Name Show",
-        description: "Mocking description",
-        type: "Mocked Type",
-        date_created: "2020-01-01T20:00:00.000Z",
-        date_scheduled: "2020-01-01T20:00:00.000Z",
-        date_ended: "2020-01-01T20:00:00.000Z",
-        participants_id: ["3fa85f64-5717-4562-b3fc-2c963f66afa6"],
+  describe("Shows routes", () => {
+    it("get single show return 1 show", async () => {
+      let value = null;
+      const show = {
+        _id: "1234",
+        name: "Test Show Name",
+        description: "Test Show Description",
+        type: "Test Show Type",
       };
 
-      mockingoose(ShowModel).toReturn(_doc, "findOne");
+      mockingoose(ShowModel).toReturn(show, "findOne");
 
-      const doc = await ShowModel.findById({ _id: _doc._id });
-      const result = JSON.parse(JSON.stringify(doc));
-
-      expect(result.name).toBe("Mock Name Show");
+      const results = await getShow(
+        { params: { _id: "1234" } },
+        {
+          send: (show) => {
+            value = show;
+          },
+        }
+      );
+      expect(value).toHaveProperty("name", "Test Show Name");
     });
   });
 });
