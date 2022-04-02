8bbb852a825b14d646c0f298ba82d8efdbfa4f82
Alexandre Costa <alexandre.costa@code.berlin>
2022-04-02 21:01:55 +0200

    refactor: create showsPayload to mock database return data

--- tests/routes/shows.test.js
+++ tests/routes/shows.test.js
@@ -17,6 +17,31 @@ const showInput = {
   date_scheduled: "2020-01-01T20:00:00.000Z",
 };
 
+const showsPayload = [
+  {
+    _id: "1234567890abcdef12345678",
+    creator_id: "12345678-90ab-cdef-1234-567890abcdef",
+    name: "Test Show Name",
+    description: "Test show description",
+    type: "test type",
+    date_created: "2020-01-01T20:00:00.000Z",
+    date_scheduled: "2020-01-01T20:00:00.000Z",
+    date_ended: "2020-01-01T21:00:00.000Z",
+    participants_id: ["3fa85f64-5717-4562-b3fc-2c963f66afa6"],
+  },
+  {
+    _id: "987654321fedcba987654321",
+    creator_id: "fedcba98-7654-3210-fedc-ba9876543210",
+    name: "Test Show Name 2",
+    description: "Test show description 2",
+    type: "test type 2",
+    date_created: "2020-01-01T20:00:00.000Z",
+    date_scheduled: "2020-02-02T20:00:00.000Z",
+    date_ended: "2020-02-02T22:00:00.000Z",
+    participants_id: ["3fa85f64-5717-4562-b3fc-2c963f66afa6"],
+  },
+];
+
 describe("Shows routes", () => {
   it("Get single show", async () => {
     // Arrange
@@ -32,12 +57,16 @@ describe("Shows routes", () => {
       }),
     };
 
-    mockingoose(ShowModel).toReturn(showInput, "findOne");
+    mockingoose(ShowModel).toReturn(
+      req.params.id == showInput._id ? showInput : undefined,
+      "findOne"
+    );
 
     let response;
 
     // Act
     await getShow(req, reply);
+    console.log(response);
 
     // Assert
     expect(response).toHaveProperty("name", "Test Show Name");
