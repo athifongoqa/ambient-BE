3abf1cbe263fc855a607308876181e3bf6bc59b3
Alexandre Costa <alexandre.costa@code.berlin>
2022-04-02 21:16:53 +0200

    test: unfinished commit

--- tests/routes/shows.test.js
+++ tests/routes/shows.test.js
@@ -1,195 +1,3 @@
-<<<<<<< HEAD
-const mockingoose = require("mockingoose");
-const ShowModel = require("../../models/show.model.js");
-const {
-  getShow,
-  getShows,
-  createShow,
-  updateShow,
-  deleteShow,
-} = require("../../controllers/shows.js");
-
-const showInput = {
-  _id: "1234567890abcdef12345678",
-  creator_id: "12345678-90ab-cdef-1234-567890abcdef",
-  name: "Test Show Name",
-  description: "Test show description",
-  type: "test type",
-  date_scheduled: "2020-01-01T20:00:00.000Z",
-};
-
-const showsPayload = [
-  {
-    _id: "1234567890abcdef12345678",
-    creator_id: "12345678-90ab-cdef-1234-567890abcdef",
-    name: "Test Show Name",
-    description: "Test show description",
-    type: "test type",
-    date_created: "2020-01-01T20:00:00.000Z",
-    date_scheduled: "2020-01-01T20:00:00.000Z",
-    date_ended: "2020-01-01T21:00:00.000Z",
-    participants_id: ["3fa85f64-5717-4562-b3fc-2c963f66afa6"],
-  },
-  {
-    _id: "987654321fedcba987654321",
-    creator_id: "fedcba98-7654-3210-fedc-ba9876543210",
-    name: "Test Show Name 2",
-    description: "Test show description 2",
-    type: "test type 2",
-    date_created: "2020-01-01T20:00:00.000Z",
-    date_scheduled: "2020-02-02T20:00:00.000Z",
-    date_ended: "2020-02-02T22:00:00.000Z",
-    participants_id: ["3fa85f64-5717-4562-b3fc-2c963f66afa6"],
-  },
-];
-
-describe("Shows routes", () => {
-  it("Get single show", async () => {
-    // Arrange
-    const req = {
-      params: {
-        id: showInput._id,
-      },
-    };
-
-    const reply = {
-      send: jest.fn().mockImplementation((res) => {
-        response = res;
-      }),
-    };
-
-    mockingoose(ShowModel).toReturn(showInput, "findOne");
-
-    let response;
-
-    // Act
-    await getShow(req, reply);
-    console.log(response);
-
-    // Assert
-    expect(response).toHaveProperty("name", "Test Show Name");
-    expect(response).toBeInstanceOf(Object);
-    expect(reply.send).toBeCalledTimes(1);
-  });
-
-  it("Get all shows", async () => {
-    // Arrange
-    const req = {};
-
-    const reply = {
-      send: jest.fn().mockImplementation((res) => {
-        response = res;
-      }),
-    };
-
-    mockingoose(ShowModel).toReturn(showsPayload, "find");
-
-    let response;
-
-    // Act
-    await getShows(req, reply);
-
-    // Assert
-    expect(response).toHaveLength(2);
-    expect(response[0]).toHaveProperty("name", "Test Show Name");
-    expect(response[1]).toHaveProperty("name", "Test Show Name 2");
-    expect(response).toBeInstanceOf(Array);
-    expect(reply.send).toBeCalledTimes(1);
-  });
-
-  it("Create a show", async () => {
-    // Arrange
-    const req = {
-      body: ({ name, creator_id } = showInput),
-    };
-
-    const reply = {
-      code: jest.fn().mockImplementation(function (code) {
-        responseCode = code;
-        return this;
-      }),
-      send: jest.fn().mockImplementation((res) => {
-        response = res;
-      }),
-    };
-
-    mockingoose(ShowModel).toReturn(showInput, "save");
-
-    let responseCode;
-    let response;
-
-    // Act
-    await createShow(req, reply);
-
-    // Assert
-    expect(responseCode).toBe(201);
-    expect(response).toHaveProperty("name", "Test Show Name");
-    expect(response).toBeInstanceOf(Object);
-    expect(reply.send).toBeCalledTimes(1);
-  });
-
-  it("Update a show", async () => {
-    // Arrange
-    const req = {
-      params: {
-        id: showInput._id,
-      },
-      body: { name: "Test Show Name Updated" },
-    };
-
-    const reply = {
-      send: jest.fn().mockImplementation((res) => {
-        response = {
-          name: req.body.name,
-          ...res,
-        };
-      }),
-    };
-
-    mockingoose(ShowModel).toReturn(showInput, "findOneAndUpdate");
-
-    let response;
-
-    // Act
-    await updateShow(req, reply);
-
-    // Assert
-    expect(response).toHaveProperty("name", "Test Show Name Updated");
-    expect(response).toBeInstanceOf(Object);
-    expect(reply.send).toBeCalledTimes(1);
-  });
-
-  it("Delete a show", async () => {
-    // Arrange
-    const req = {
-      params: {
-        id: showInput._id,
-      },
-    };
-
-    const reply = {
-      send: jest.fn().mockImplementation((res) => {
-        response = res;
-      }),
-    };
-
-    mockingoose(ShowModel).toReturn(showInput, "findOneAndRemove");
-
-    let response;
-
-    // Act
-    await deleteShow(req, reply);
-
-    // Assert
-    expect(response).toHaveProperty(
-      "message",
-      `${req.params.id} has been deleted`
-    );
-    expect(response).toBeInstanceOf(Object);
-    expect(reply.send).toBeCalledTimes(1);
-  });
-});
-=======
 /* eslint-disable no-underscore-dangle */
 /* global jest, it, describe, expect */
 
@@ -247,10 +55,15 @@ describe('Shows routes', () => {
     };
 
     let response;
+    let responseCode = 200;
 
     const reply = {
       send: jest.fn().mockImplementation((res) => {
         response = res;
+      }),
+      status: jest.fn().mockImplementation(function (code) {
+        responseCode = code;
+        return this;
       }),
     };
 
@@ -265,6 +78,44 @@ describe('Shows routes', () => {
 
     // Assert
     expect(response).toHaveProperty('name', 'Test Show Name');
+    expect(responseCode).toBe(200);
+    expect(response).toBeInstanceOf(Object);
+    expect(reply.send).toBeCalledTimes(1);
+  });
+
+  it('Get single show with invalid id', async () => {
+    // Arrange
+    const req = {
+      params: {
+        id: '123',
+      },
+    };
+
+    let response;
+    let responseCode = 200;
+
+    const reply = {
+      send: jest.fn().mockImplementation((res) => {
+        response = res;
+      }),
+      status: jest.fn().mockImplementation(function (code) {
+        responseCode = code;
+        return this;
+      }),
+    };
+
+    mockingoose(ShowModel).toReturn(
+      req.params.id === showInput._id ? showInput : undefined,
+      'findOne',
+    );
+
+    // Act
+    await getShow(req, reply);
+    // console.log(response);
+
+    // Assert
+    expect(response).not.toHaveProperty('name');
+    expect(responseCode).toBe(404);
     expect(response).toBeInstanceOf(Object);
     expect(reply.send).toBeCalledTimes(1);
   });
@@ -388,4 +239,3 @@ describe('Shows routes', () => {
     expect(reply.send).toBeCalledTimes(1);
   });
 });
->>>>>>> 7f9d575 (style: lint show tests)
