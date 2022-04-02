a2269abf3a11adf3689147ef4f93ecd539eb143b
Alexandre Costa <alexandre.costa@code.berlin>
2022-04-02 02:44:07 +0200

    test: get a single show

--- test/shows.integration.js
+++ test/shows.integration.js
@@ -13,10 +13,22 @@ describe("shows integration tests", () => {
     await app.close();
   });
 
-  it("default shows route", async () => {
+  it("Get all shows", async () => {
     const { statusCode, body } = await request(app.server).get("/api/shows/");
 
     assert.equal(statusCode, 200);
+    assert.equal(Array.isArray(body), true);
     assert.notEqual(body.length, 0);
+    assert.notEqual(body[0].description, undefined);
+  });
+
+  it("Get a single show", async () => {
+    const { statusCode, body } = await request(app.server).get(
+      "/api/shows/622d46cd755a74341a120491"
+    );
+
+    assert.equal(statusCode, 200);
+    assert.equal(body.name, "No name show 10");
+    assert.equal(typeof body, "object");
   });
 });
