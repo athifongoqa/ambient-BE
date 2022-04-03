96417c11fde7961ad3be5e132ac8842e7b2ad48d
Athi Fongoqa <fongoqaathi@gmail.com>
2022-04-03 05:52:32 +0200

    build: add user endpoints' schemas

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,4 @@
-const User = require("../models/user");
+const User = require("../models/user.model");
 const boom = require('boom')
 const validator = require("validator");
 
--- routes/api/users/index.js
+++ routes/api/users/index.js
@@ -1,18 +1,19 @@
 "use strict";
 const userController = require("../../../controllers/users");
+const {getUsersOpts, getUserOpts, postUserOpts, deleteUserOpts, updateUserOpts} = require("./schema")
 
 module.exports = async function (fastify, opts) {
   fastify.register(require("fastify-prettier"), {
     alwaysOn: true,
   });
 
-  fastify.get("/", userController.getAllUsers);
+  fastify.get("/", getUsersOpts.schema, userController.getAllUsers);
 
-  fastify.get("/:username", userController.getSingleUser);
+  fastify.get("/:username", getUserOpts.schema, userController.getSingleUser);
 
-  fastify.post("/", userController.addNewUser);
+  fastify.post("/", postUserOpts.schema, userController.addNewUser);
 
-  fastify.patch("/:id", userController.updateSingleUser);
+  fastify.patch("/:id", updateUserOpts.schema, userController.updateSingleUser);
 
-  fastify.delete("/:id", userController.deleteSingleUser);
+  fastify.delete("/:id", deleteUserOpts.schema, userController.deleteSingleUser);
 };
