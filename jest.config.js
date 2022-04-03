38c9a88bc13a07c3a2decf05caeb840c148441d0
Alexandre Costa <alexandre.costa@code.berlin>
2022-04-03 15:48:06 +0200

    test: change jest config file that prevented shows.test to pass

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,9 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-    testEnvironment: 'node',
-    testMatch: ["**/**/*.test.js"],
-    verbose: true,
-    forceExit: true,
-    clearMocks: true,
-    resetMocks: true,
-    restoreMocks: true,
-  };
+  testEnvironment: "node",
+  testMatch: ["**/**/*.test.js"],
+  verbose: true,
+  forceExit: true,
+  clearMocks: true,
+  restoreMocks: true,
+};
