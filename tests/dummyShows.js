a08c4af766e106fc58b7911eea4cf8afa5a58b79
Alexandre Costa <alexandre.costa@code.berlin>
2022-04-10 15:35:34 +0200

    refactor: move dummy data to new module, for reuse

---
+++ tests/dummyShows.js
@@ -0,0 +1,38 @@
+const showInput = {
+  _id: expect.any(String),
+  creator_id: '12345678-90ab-cdef-1234-567890abcdef',
+  name: 'Test Show Name',
+  description: 'Test show description',
+  type: 'test type',
+  date_scheduled: '2020-01-01T20:00:00.000Z',
+};
+
+const showsPayload = [
+  {
+    _id: expect.any(String),
+    creator_id: '12345678-90ab-cdef-1234-567890abcdef',
+    name: 'Test Show Name',
+    description: 'Test show description',
+    type: 'test type',
+    date_created: '2020-01-01T20:00:00.000Z',
+    date_scheduled: '2020-01-01T20:00:00.000Z',
+    date_ended: '2020-01-01T21:00:00.000Z',
+    participants_id: ['3fa85f64-5717-4562-b3fc-2c963f66afa6'],
+  },
+  {
+    _id: expect.any(String),
+    creator_id: 'fedcba98-7654-3210-fedc-ba9876543210',
+    name: 'Test Show Name 2',
+    description: 'Test show description 2',
+    type: 'test type 2',
+    date_created: '2020-01-01T20:00:00.000Z',
+    date_scheduled: '2020-02-02T20:00:00.000Z',
+    date_ended: '2020-02-02T22:00:00.000Z',
+    participants_id: ['3fa85f64-5717-4562-b3fc-2c963f66afa6'],
+  },
+];
+
+module.exports = {
+  showInput,
+  showsPayload,
+};
--- tests/routes/shows.test.js
+++ tests/routes/shows.test.js
@@ -11,39 +11,7 @@ const {
   deleteShow,
 } = require('../../controllers/shows');
 
-const showInput = {
-  _id: '1234567890abcdef12345678',
-  creator_id: '12345678-90ab-cdef-1234-567890abcdef',
-  name: 'Test Show Name',
-  description: 'Test show description',
-  type: 'test type',
-  date_scheduled: '2020-01-01T20:00:00.000Z',
-};
-
-const showsPayload = [
-  {
-    _id: '1234567890abcdef12345678',
-    creator_id: '12345678-90ab-cdef-1234-567890abcdef',
-    name: 'Test Show Name',
-    description: 'Test show description',
-    type: 'test type',
-    date_created: '2020-01-01T20:00:00.000Z',
-    date_scheduled: '2020-01-01T20:00:00.000Z',
-    date_ended: '2020-01-01T21:00:00.000Z',
-    participants_id: ['3fa85f64-5717-4562-b3fc-2c963f66afa6'],
-  },
-  {
-    _id: '987654321fedcba987654321',
-    creator_id: 'fedcba98-7654-3210-fedc-ba9876543210',
-    name: 'Test Show Name 2',
-    description: 'Test show description 2',
-    type: 'test type 2',
-    date_created: '2020-01-01T20:00:00.000Z',
-    date_scheduled: '2020-02-02T20:00:00.000Z',
-    date_ended: '2020-02-02T22:00:00.000Z',
-    participants_id: ['3fa85f64-5717-4562-b3fc-2c963f66afa6'],
-  },
-];
+const { showInput, showsPayload } = require('../dummyShows');
 
 describe('Shows routes', () => {
   it('Get single show', async () => {
