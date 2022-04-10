a6867991c32e814435cf22bd6549bb2e6f5ded7d
Alexandre Costa <alexandre.costa@code.berlin>
2022-04-10 18:26:17 +0200

    refactor: remove unnecessary comments

--- routes/api/shows/index.js
+++ routes/api/shows/index.js
@@ -16,18 +16,13 @@ module.exports = async function (fastify) {
     reply.code(500).send({ message: 'The server returned an error' });
   });
 
-  // Get all shows
   fastify.get('/', getShowsOpts);
 
-  // Get single show
   fastify.get('/:id', getShowOpts);
 
-  // Create a new show
   fastify.post('/', postShowOpts);
 
-  // Delete a show
   fastify.delete('/:id', deleteShowOpts);
 
-  // Update a show
   fastify.put('/:id', updateShowOpts);
 };
