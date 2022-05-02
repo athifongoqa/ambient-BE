a40ca00cbbd0194e95a7a009dad4084ca583a93f
Athi Fongoqa <fongoqaathi@gmail.com>
2022-05-02 14:52:19 +0200

    refactor: access seperate getAccessToken handler

--- tests/routes/users.test.js
+++ tests/routes/users.test.js
@@ -1,13 +1,11 @@
 const request = require('supertest');
-const { build } = require('../helper');
+const { build, getAccessToken } = require('../helper');
 const db = require('../testdb');
 const users = require('../../controllers/users');
-const Fastify = require('fastify')
-const jwt = require('../../plugins/jwt')
 const user = require('../../controllers/users')
 
 let app;
-let fastify;
+let adminAccessToken;
 
 function createDummyUser(username, displayName, email, avatar, role) {
   return {
@@ -23,7 +21,7 @@ const userPayLoad = {
   _id: expect.any(String),
   username: 'dummyOne',
   displayName: 'Dumb',
-  email: 'dummyMemberOne23.dummy@code.berlin',
+  email: 'dummyMember123.dummy@code.berlin',
   avatar: 'img.ip/464558.jpg',
   role: 'member',
   followers: [],
@@ -41,12 +39,10 @@ adminUser = createDummyUser(
 beforeAll(async () => {
   app = await build();
   await db.connect();
+  adminAccessToken = await getAccessToken(adminUser);
 }, 15000);
 
 beforeEach(async () => {
-  fastify = Fastify()
-  fastify.register(jwt)
-  await fastify.ready()  
   await db.clearDatabase();
 });
 
@@ -72,13 +68,7 @@ describe('E2E User endpoints', () => {
     'member',
   );
   
-  it('should POST a single user', async () => {
-    const adminAccessToken = await fastify.signIn({body: adminUser}).then(async (token) => {
-      return token
-    }).catch((err) => {
-      return err
-    })
-    
+  it('should POST a single user', async () => {    
     const { statusCode, body } = await request(app.server)
     .post('/api/users/')
     .send(dummyMemberOne)
@@ -91,12 +81,8 @@ describe('E2E User endpoints', () => {
   });
   
   it('should GET all users', async () => {
+    await users.addNewUser({body: dummyMemberOne})
     await users.addNewUser({body: dummyMemberTwo})
-    const adminAccessToken = await fastify.signIn({body: adminUser}).then(async (token) => {
-      return token
-    }).catch((err) => {
-      return err
-    })
 
     const { body, statusCode } = await request(app.server)
     .get('/api/users/')
@@ -104,17 +90,13 @@ describe('E2E User endpoints', () => {
     
     expect(statusCode).toBe(200);
     expect(body).toBeInstanceOf(Object);
-    expect(body.allUsers[0]).toMatchObject(dummyMemberTwo);
-    expect(body.allUsers[1]).toMatchObject(adminUser);
+    console.log(body)
+    expect(body.allUsers[0]).toMatchObject(dummyMemberOne);
+    expect(body.allUsers[1]).toMatchObject(dummyMemberTwo);
   });
   
   it('should GET a single user', async () => {
     await user.addNewUser({ body: dummyMemberOne });
-    const adminAccessToken = await fastify.signIn({body: adminUser}).then(async (token) => {
-      return token
-    }).catch((err) => {
-      return err
-    })
     
     const { body, statusCode } = await request(app.server)
     .get(`/api/users/${dummyMemberOne.username}`)
@@ -126,11 +108,6 @@ describe('E2E User endpoints', () => {
     
     it('should DELETE a single user', async () => {
       const newUser = await user.addNewUser({ body: dummyMemberTwo });
-      const adminAccessToken = await fastify.signIn({body: adminUser}).then(async (token) => {
-        return token
-      }).catch((err) => {
-        return err
-      })
 
       const { body, statusCode } = await request(app.server)
       .delete(`/api/users/${newUser._id}`)
@@ -144,11 +121,6 @@ describe('E2E User endpoints', () => {
   it('should UPDATE a single user', async () => {
     const newUser = await user.addNewUser({ body: dummyMemberTwo });
     const newDisplayName = 'Ronald';
-    const adminAccessToken = await fastify.signIn({body: adminUser}).then(async (token) => {
-      return token
-    }).catch((err) => {
-      return err
-    })
 
     const { body, statusCode } = await request(app.server)
       .patch(`/api/users/${newUser._id}`)
