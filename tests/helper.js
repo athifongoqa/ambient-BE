c38fe58dcda538b9015d113fb46f74a2a2693311
Athi Fongoqa <fongoqaathi@gmail.com>
2022-05-02 14:33:14 +0200

    refactor: remove comments

--- tests/helper.js
+++ tests/helper.js
@@ -6,21 +6,14 @@ const boom = require('boom');
 
 const AppPath = path.join(__dirname, '../', 'app.js');
 
-// Fill in this config with all the configurations
-// needed for testing the application
 function config() {
   return {};
 }
 
-// automatically build and tear down our instance
 async function build() {
   try {
-    // you can set all the options supported by the fastify CLI command
     const argv = [AppPath];
 
-    // fastify-plugin ensures that all decorators
-    // are exposed for testing purposes, this is
-    // different from the production setup
     const app = await buildApplication(argv, config());
     return app;
   } catch (err) {
