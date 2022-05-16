bc4a410d13408985359e6f4dc5208891debb2d28
Alexandre Costa <alexandre.costa@code.berlin>
2022-05-17 00:21:14 +0200

    refactor: lint eqeqeq

--- controllers/shows.js
+++ controllers/shows.js
@@ -2,7 +2,7 @@ const Show = require('../models/show.model');
 
 const getShows = async (req, reply) => {
   const shows = await Show.find();
-  shows.length != 0 ? reply.send(shows) : reply.code(204).send();
+  shows.length ? reply.send(shows) : reply.code(204).send();
 };
 
 const getShow = async function (req, reply) {
